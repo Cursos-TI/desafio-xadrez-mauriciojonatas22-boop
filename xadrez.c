393528e5ceec910630eca255903ced25c487c78e
mauriciojonatas22-boop <mauriciojonatas22@gmail.com>
2025-09-10 00:44:28 +0000

    criei o plintf.

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int main() {printf("desafio xadrex!|n");
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
